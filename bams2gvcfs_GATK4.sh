94752064f791bc1da7ade3f6b1ead30d6bcad7b3
Lucas <35809839+lucasrocmoreira@users.noreply.github.com>
2022-02-02 16:30:37 -0500

    Update bams2gvcfs_GATK4.sh

--- bams2gvcfs_GATK4.sh
+++ bams2gvcfs_GATK4.sh
@@ -43,8 +43,8 @@ export _JAVA_OPTIONS=-Djava.io.tmpdir=$tmpdir
 
 # -b tells the cluster to execute the job as a binary. This saves a lot of time because the cluster will not transfer your script but instead just the pointer to the path.
 # -p defines the priority of the job
-# -V declares that	all environment	variables in the qsub command's environment	are to be exported to the batch	job.
-# -j declares if the standard error stream	of the job will be merged with the standard output stream of 
+# -V declares that all environment variables in the qsub command's environment	are to be exported to the batch job.
+# -j declares if the standard error stream of the job will be merged with the standard output stream of 
 # -cwd tells that the job should be executed in the same directory that qsub was called.
 qsub -l h_vmem=32g -l h_rt=48:00:00 -b y -p -10 -N $name -cwd -o GVCF/$name.haplotype_caller.ERC.out -j y -V $GATK_EXECUTABLE --java-options "-Xmx10g" HaplotypeCaller --tmp-dir $tmpdir -R $1 -I $2 -O GVCF/$name.g.vcf.gz -ERC GVCF -GQB 20 -GQB 100 --min-pruning 2 #-GQB specifies the exclusive upper bounds for reference confidence GQ bands
 
