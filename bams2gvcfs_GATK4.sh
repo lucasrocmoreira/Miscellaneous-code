84526ede5819fd7749d2c14329ad0477c9673fa7
Lucas <35809839+lucasrocmoreira@users.noreply.github.com>
2023-03-09 14:56:36 -0500

    Update bams2gvcfs_GATK4.sh

--- bams2gvcfs_GATK4.sh
+++ bams2gvcfs_GATK4.sh
@@ -7,7 +7,7 @@ reuse Java-1.8
 use UGER
 
 GATK_EXECUTABLE=/seq/vgb/software/gatk/gatk-4.2.2.0/gatk
-tmpdir=/home/unix/lmoreira/vgb/gorilla/tmp
+tmpdir=/home/unix/lmoreira/vgb/gorilla/High_depth_WGS/tmp
 
 Help()
 {
@@ -15,9 +15,10 @@ Help()
    echo
    echo "Script to call genotypes with GATK4."
    echo
-   echo "Syntax: bams2gvcf_GATK4.sh REF BAM_FILE"
+   echo "Syntax: bams2gvcf_GATK4.sh REF BAM_FILE OUTPUT_DIRECTORY"
    echo "REF = reference genome file (.fasta)"
    echo "BAM_FILE = aligned bam file (.bam)"
+   echo "OUTPUT_DIRECTORY = path to output directory"
    echo
 }
 
@@ -33,8 +34,9 @@ fi
 file=`basename $2`
 name=`echo $file | cut -d '.' -f1`
 
-if [ ! -d GVCF/ ]; then
-  mkdir GVCF/;
+OUTPUT_DIRECTORY=$3
+if [ ! -d $OUTPUT_DIRECTORY/ ]; then
+  mkdir $OUTPUT_DIRECTORY/;
 fi
 
 export _JAVA_OPTIONS=-Djava.io.tmpdir=$tmpdir
@@ -44,4 +46,4 @@ export _JAVA_OPTIONS=-Djava.io.tmpdir=$tmpdir
 # -V declares that all environment variables in the qsub command's environment	are to be exported to the batch job.
 # -j declares if the standard error stream of the job will be merged with the standard output stream of 
 # -cwd tells that the job should be executed in the same directory that qsub was called.
-qsub -l h_vmem=32g -l h_rt=48:00:00 -b y -p -10 -N $name -cwd -o GVCF/$name.haplotype_caller.ERC.out -j y -V $GATK_EXECUTABLE --java-options "-Xmx10g" HaplotypeCaller --tmp-dir $tmpdir -R $1 -I $2 -O GVCF/$name.g.vcf.gz -ERC GVCF -GQB 20 -GQB 100 --min-pruning 2 #-GQB specifies the exclusive upper bounds for reference confidence GQ bands
+qsub -l h_vmem=32g -l h_rt=48:00:00 -b y -p -10 -N $name -cwd -o GVCF/$name.haplotype_caller.ERC.out -j y -V $GATK_EXECUTABLE --java-options "-Xmx32g" HaplotypeCaller --tmp-dir $tmpdir -R $1 -I $2 -O GVCF/$name.g.vcf.gz -ERC GVCF -GQB 20 -GQB 100 --min-pruning 2 #-GQB specifies the exclusive upper bounds for reference confidence GQ bands
